3a69cdfd5b69c1aea243b2a61e569eb3306d5ec0
FraIanna <francescoiannaccone9@gmail.com>
2024-06-18 14:07:48 +0200

    Consegna 18-06

---
+++ Giorno 2/Persona.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giorno_2
+{
+    internal class Persona
+    {
+        private string name;
+        private string surname;
+        private int age;
+
+        public string Name { get { return name; } set { name = value; } }  
+        public string Surname { get { return surname; } set { surname = value; } }
+        public int Age { get { return age;   } set { age = value; } }
+
+        public void getName(string var) 
+        {
+            Console.WriteLine("Il mio nome è: " + var);
+        }
+        public void getSurname(string var) 
+        {
+            Console.WriteLine("Il mio cognome è: " + var);
+        }
+        public void getDetails(string var1, string var2, int var3) 
+        {
+            Console.WriteLine("Il mio nome è: " + var1 + ", invece il mio cognome è: " + var2 + ", ho " + var3 + " anni");
+        }
+
+    }
+}
---
+++ Giorno 2/Program.cs
@@ -0,0 +1,17 @@
+﻿using Giorno_2;
+
+Console.WriteLine("ciao");
+
+Persona a = new()
+{
+    Name = "Jhon",
+    Surname = "Benson",
+    Age = 30,
+};
+
+
+a.getName(a.Name);
+a.getSurname(a.Surname);
+a.getDetails(a.Name, a.Surname, a.Age);
+
+
