c162d83393e1abeaa2f6954abe82c300ec673e9f
FraIanna <francescoiannaccone9@gmail.com>
2024-06-19 16:46:21 +0200

    Consegna 19-06

---
+++ Giorno 3/ContoCorrente.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giorno_3
+{
+    internal class ContoCorrente
+    {
+        public string NomeIntestatario { get; private set; }
+        public string NumeroConto { get; private set; }
+        public decimal Saldo { get; private set; }
+        private bool ContoAperto { get; set; }
+
+        public ContoCorrente (string nomeIntestatario, string numeroConto)
+        {
+            NomeIntestatario = nomeIntestatario;
+            NumeroConto = numeroConto;
+            Saldo = 0;
+            ContoAperto = false;
+        }
+
+        public void ApriConto (decimal primoVersamento)
+        {
+            if (ContoAperto) {
+                Console.WriteLine("Il conto è già stato aperto.");
+                return;
+            }
+            if(primoVersamento < 1000)
+            {
+                Console.WriteLine("Attenzione! Il primo versamento deve essere almeno di 1000$");
+                return;
+            }
+
+            Saldo = primoVersamento;
+            ContoAperto = true;
+            Console.WriteLine($"Conto aperto correttamente, con un versamento di {primoVersamento}$");
+        } 
+        
+        public void Versamento(decimal importo)
+        {
+            if (!ContoAperto)
+            {
+                Console.WriteLine("Il conto non è ancora stato aperto");
+                return;
+            } 
+            Saldo += importo;
+            Console.WriteLine($"Versamento di {importo}$ effettuato con successo");
+
+        }
+
+        public void Prelievo (decimal importo)
+        {
+            if (!ContoAperto)
+            {
+                Console.WriteLine("Il conto non è ancora stato aperto");
+                return;
+            }
+
+            if (importo > Saldo) {
+
+                Console.WriteLine($"Saldo insufficiente per prelevare {importo}$ ");
+                return;
+            }
+
+            Saldo -= importo;
+            Console.WriteLine($"Versamento di {importo}$ effettuato con successo");
+
+        }
+    }
+}
---
+++ Giorno 3/Program.cs
@@ -0,0 +1,96 @@
+﻿namespace Giorno_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Test classe ContoCorrente
+
+            ContoCorrente conto = new ContoCorrente("Mario Rossi", "IT23470924098");
+            
+            conto.ApriConto(500);
+            // importo inferiore a 1000
+
+            Console.WriteLine("--------------------");
+
+            conto.ApriConto(1200);
+            //importo superiore a 1000
+
+            Console.WriteLine("--------------------");
+
+            conto.Versamento(1200);
+            // versamento valido
+
+            Console.WriteLine("--------------------");
+
+            conto.Prelievo(2000);
+            // prelievo non valido
+
+            Console.WriteLine("--------------------");
+
+            conto.Prelievo(1000);
+            // prelievo valido
+
+            Console.WriteLine("--------------------");
+
+            Console.WriteLine($"Il saldo del conto è: {conto.Saldo}$");
+
+            Console.WriteLine("---------Fine--------");
+
+            //--------------++++++++++++++--------------//
+
+            // Eserizio 2
+            string[] nomi = { "Antonio", "Michele", "Ilaria", "Giulia", };
+            Console.WriteLine("Inserisci il nome da cercare: ");
+            string nomeDaCercare = Console.ReadLine();
+
+            bool found = false;
+            foreach (string nome in nomi) { 
+            
+                if (nome.Equals(nomeDaCercare, StringComparison.OrdinalIgnoreCase))
+                { 
+                    found = true; 
+                    break;
+                }
+
+            }
+            if (found) {
+
+                Console.WriteLine("È presente nell'array");
+                
+            } 
+            else
+            {
+                Console.WriteLine("Non è presente nell'array");
+            }
+
+            // Esercizio 3
+
+            Console.WriteLine("Inserisci la dimensione dell'array:");
+            
+            int dimensione = int.Parse(Console.ReadLine());
+
+            int[] numeri = new int[dimensione];
+
+            for (int i = 0; i < dimensione; i++)
+            {
+                Console.WriteLine("Inserisci un numero intero:");
+                numeri[i] = int.Parse(Console.ReadLine());
+            }
+
+            int somma = 0;
+            foreach (int numero in numeri)
+
+            {
+                somma += numero;
+            }
+
+            double media = (double)somma / dimensione;
+
+            Console.WriteLine("La somma di tutti i numeri è: " + somma);
+            Console.WriteLine("La media aritmetica di tutti i numeri è: " + media);
+        }
+        
+    }
+    
+}
