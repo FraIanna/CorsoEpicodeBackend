1e58e0e802211bc794e27735bf106415475be3b2
FraIanna <francescoiannaccone9@gmail.com>
2024-06-20 16:45:57 +0200

    Consegna 20-06

---
+++ Giorno 4/Program.cs
@@ -0,0 +1,74 @@
+﻿namespace Giorno_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.Clear();
+                Console.WriteLine("===============OPERAZIONI==============");
+                Console.WriteLine("Scegli l'operazione da effettuare:");
+                Console.WriteLine("1.: Login");
+                Console.WriteLine("2.: Logout");
+                Console.WriteLine("3.: Verifica ora e data di login");
+                Console.WriteLine("4.: Lista degli accessi");
+                Console.WriteLine("5.: Esci");
+                Console.WriteLine("========================================");
+
+                char choice = Choice();
+
+                switch (choice)
+                {
+                    case '1':
+                        Utente.Login();
+                        break;
+                    case '2':
+                        Utente.Logout();
+                        break;
+                    case '3':
+                        Utente.VerifyLogin();
+                        break;
+                    case '4':
+                        Utente.ListUsersLogin();
+                        break;
+                    case '5':
+                        exit = true;
+                        break;
+                    default:
+                        Console.WriteLine("Operazione non valida. Premi un tasto per continuare.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+            
+            static char Choice()
+            {
+                char answer;
+                do 
+                {
+                    string input = Console.ReadLine();
+                    if (!string.IsNullOrEmpty(input) && input.Length == 1)
+                    {
+                        answer = input[0];
+                    }
+                    else
+                    {
+                        answer = '0';
+                    }
+                } 
+                while (answer < '1' || answer > '5');
+                return answer;
+            }
+
+            Utente.Login();
+            Utente.UserLogout();
+            Utente.VerifyLogin();
+            Utente.ListUsersLogin();
+
+        }
+    }
+}
---
+++ Giorno 4/Utente.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Giorno_4
+{
+    internal class Utente
+    {
+        private static string _username;
+        private static string _password;
+
+
+        public static DateTime LastLoginTime { get; set; }
+        public static List <string> Usernames { get; } = new List<string>();
+
+
+        public static bool isLoggedIn() 
+        { return !string.IsNullOrEmpty(_username); }
+
+
+        public static bool Login(string username, string password, string confirmPassword)
+        {
+            if (!string.IsNullOrEmpty(username) && password == confirmPassword)
+            {
+                _username = username;
+                _password = password;
+                LastLoginTime = DateTime.Now;
+                Usernames.Add($"{LastLoginTime}: {_username}");
+                if (Usernames.Count > 10)
+                {
+                    Usernames.RemoveAt(0);
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public static bool Logout ()
+         {
+            if (isLoggedIn())
+            {
+                _username = null;
+                _password = null;
+                return true;
+            }
+             return false;
+         }
+
+
+        public static void Login()
+        {
+            Console.WriteLine("Inserisci username: ");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Inserisci password: ");
+            string password = Console.ReadLine();
+            Console.WriteLine("Conferma password: ");
+            string confirmPassword = Console.ReadLine();
+
+            if (Utente.Login(username, password, confirmPassword))
+            {
+                Console.WriteLine("Login effettuato con successo!");
+            }
+            else
+            {
+                Console.WriteLine("Errore nel login.Verificaidatiinseriti.");
+            }
+            Console.WriteLine("Premi un tasto per continuare.");
+            Console.ReadKey();
+
+        }
+
+        public static void UserLogout()
+        {
+            if (Utente.Logout())
+            {
+                Console.WriteLine("Logout effettuato con successo!");
+                }
+            else
+            {
+                Console.WriteLine("Nessun utente attualmeloggato.");
+            }
+            Console.WriteLine("Premi un tasto per continuare.");
+            Console.ReadKey();
+        }
+
+        public static void VerifyLogin()
+        {
+            if (Utente.isLoggedIn())
+            {
+                Console.WriteLine($"Ultimo login effettuato il: {Utente.LastLoginTime}");
+            }
+            else
+            {
+                Console.WriteLine("Nessun utente attualmente loggato.");
+            }
+            Console.WriteLine("Premi un tasto per continuare.");
+            Console.ReadKey();
+        }
+
+        public static void ListUsersLogin()
+        {
+            Console.WriteLine("Lista degli ultimi 10 accessi:");
+            foreach (var accesso in Utente.Usernames)
+            {
+                Console.WriteLine(accesso);
+            }
+            Console.WriteLine("Premi un tasto per continuare.");
+            Console.ReadKey();
+        }
+    }
+}
