ffcc228c01217da060f959719633cf763337f082
Francesco Iannaccone <164211191+FraIanna@users.noreply.github.com>
2024-07-02 16:04:48 +0200

    Add files via upload

---
+++ Esercizio 02-07/Consegna 02-07 SqlQuery.sql
@@ -0,0 +1,28 @@
+﻿-- Esercizio 1&2
+Select * from Products
+-- where UnitsInStock >= 40
+where City = 'London'
+
+-- Esercizio 3
+-- Select * from Employees
+-- where City = 'London';
+
+-- Esercizio 4
+-- Select * from Orders
+-- order by Freight DESC;
+
+-- Esercizio 5
+-- Select * from Orders
+-- where Freight > 90 AND Freight < 200;
+
+-- Esercizio 6
+-- Select * from Products
+-- WHERE CategoryID = 1;
+
+-- Esercizio 7
+-- Select * from OrderDetails
+-- WHERE Discount > 0;
+
+-- Esercizio 8 
+-- Select * from Orders
+-- WHERE CustomerID = 'BOTTM' AND Freight > 50;
