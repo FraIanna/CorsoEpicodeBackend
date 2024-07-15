f0441f8536031b3b556b0e06e3802fba3e5084bd
FraIanna <francescoiannaccone9@gmail.com>
2024-07-15 16:44:42 +0200

    Prino commit Pratica S5

---
+++ S2-Settimanale/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace S2_Settimanale.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
